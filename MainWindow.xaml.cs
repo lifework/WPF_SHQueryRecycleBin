6c407eec573e817a9dd0581a588f93ef74a6f50b
Kazushige Kobayashi <kobayashi@lifework.tech>
2022-06-23 13:06:25 +0900

    ViewModel の追加

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,7 @@ namespace WPF_SHQueryRecycleBin
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = new MainWindowViewModel();
         }
     }
 }
---
+++ MainWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_SHQueryRecycleBin
+{
+    internal class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public long RecycleBinSize { get; set; }
+
+        public MainWindowViewModel()
+        {
+            RecycleBinSize = 10000;
+        }
+    }
+}
